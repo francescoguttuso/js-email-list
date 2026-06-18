ee53934d82a389972f96e5bd60ca437218a991e3
francescoguttuso <francescoguttuso@gmail.com>
2026-06-18 09:01:19 +0200

    resolve bug script axios

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((response) => {
-        const email = response.data.response;
-        const emailList = document.querySelector("ul.list-group");
-        const newEmailItem = document.createElement("li");
+const emailList = document.querySelector("ul.list-group");
+const button = document.querySelector("button");
 
-        newEmailItem.classList.add("list-group-item");
-        newEmailItem.textContent = email;
-        emailList.appendChild(newEmailItem);
-    })
-    .catch((error) => {
-        console.error("Si è verificato un errore:", error);
-        // Opzionale: mostra un messaggio di errore all'utente nella pagina
-    });
+const generateTenEmail = () => {
+    emailList.innerHTML = "";
+    // Genera 10 email casuali
+    for (let i = 0; i < 10; i++) {
+        // Effettua una richiesta GET all'API per ottenere un'email casuale
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                const email = response.data.response;
+
+                const newEmailItem = document.createElement("li");
+
+                newEmailItem.classList.add("list-group-item");
+                newEmailItem.textContent = email;
+                emailList.appendChild(newEmailItem);
+            })
+            .catch((error) => {
+                console.error("Si è verificato un errore:", error);
+                // Opzionale: mostra un messaggio di errore all'utente nella pagina
+            });
+    }
+};
+// Genera le email iniziali al caricamento della pagina
+generateTenEmail();
+// Aggiungi un listener al pulsante per generare nuove email quando viene cliccato
+button.addEventListener("click", generateTenEmail);
